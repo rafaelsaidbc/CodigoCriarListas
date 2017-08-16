5fcec640396db42d8d84a58c6022e4ab34eeffdb
Rafael Said <rafaelsaidbc@yahoo.com.br>
2017-08-15 22:25:53 -0300

    Initial commit

---
+++ app/src/main/java/android/example/com/santacruztests/MainActivity.java
@@ -0,0 +1,61 @@
+package android.example.com.santacruztests;
+
+/**
+ * Created by Rafael on 14/08/2017.
+ */
+
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_info_jogadores);
+
+        ArrayList<Jogador> jogadores = new ArrayList<Jogador>();
+        jogadores.add(new Jogador(R.drawable.ricardo_corpo, "Nome: Ricardo Luis dos Santos Rodrigues",
+                "Apelido: Ricardinho", "Data de Nascimento: 20/12/1986", "Número da camisa: 17",
+                "Ano de ingresso: 2005"));
+
+        // Create an {@link ArrayAdapter}, whose data source is a list of Strings. The
+        // adapter knows how to create layouts for each item in the list, using the
+        // simple_list_item_1.xml layout resource defined in the Android framework.
+        // This list item layout contains a single {@link TextView}, which the adapter will set to
+        // display a single word.
+        JogadorAdapter adapter = new JogadorAdapter(this, jogadores);
+
+        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
+        // There should be a {@link ListView} with the view ID called list, which is declared in the
+        // lista_info_jogadores.xml file.
+        ListView listView = (ListView) findViewById(R.id.lista_info_jogadores);
+
+        // Make the {@link ListView} use the {@link ArrayAdapter} we created above, so that the
+        // {@link ListView} will display list items for each word in the list of words.
+        // Do this by calling the setAdapter method on the {@link ListView} object and pass in
+        // 1 argument, which is the {@link ArrayAdapter} with the variable name itemsAdapter.
+        listView.setAdapter(adapter);
+    }
+
+}
